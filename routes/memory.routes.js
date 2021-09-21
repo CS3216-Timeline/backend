c244f75cf41787ad7db8a9e19c1a58f56e69f4d1
pr4aveen <ajjuu98@gmail.com>
2021-09-21 17:42:23 +0800

    Add memory routes

--- routes/memory.routes.js
+++ routes/memory.routes.js
@@ -1,67 +1,189 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const { BadRequestError } = require("../errors/errors");
-const auth = require("../middleware/auth");
-const passport = require('passport');
+const { check, oneOf, validationResult } = require("express-validator");
+const { BadRequestError, UnauthorizedError } = require("../errors/errors");
+const passport = require("passport");
 const MemoryService = require("../services/MemoryService");
 const memoryService = new MemoryService();
 
-// Create a memory
-router.post('/',
-  passport.authenticate(['jwt'], { session: false }),
+const multer = require("multer");
+const LineService = require("../services/LineService");
+const lineService = new LineService();
+const upload = multer();
+
+router.post(
+  "/",
+  passport.authenticate(["jwt"], { session: false }),
+  upload.array("images", 10),
   [
-    //TODO: Check which fields are necessary 
+    //TODO: Check which fields are necessary
     check("title", "Title of memory cannot be blank").notEmpty(),
     check("description", "Description cannot be blank").exists(),
-    check("media", "Media cannot be blank").exists(),
     check("line", "Line cannot be blank").exists(),
     check("latitude", "Latitude cannot be blank").exists(),
-    check("longitude", "Longitude cannot be blank").exists()
+    check("longitude", "Longitude cannot be blank").exists(),
   ],
   async (req, res, next) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      throw new BadRequestError(errors.array().map(err => err.msg).join(', '))
-    }
-
-    const userId = req.user.userId;
-    const { title } = req.body;
-
     try {
-      const memory = await memoryService.createMemory(userId, title);
-      res.status(200).json({ 
-        memory
-      })
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new BadRequestError(
+          errors
+            .array()
+            .map((err) => err.msg)
+            .join(", ")
+        );
+      }
+
+      const userId = req.user.userId;
+      const title = req.body["title"];
+      const lineId = req.body["line"];
+      const description = req.body["description"];
+      const latitude = req.body["latitude"];
+      const longitude = req.body["longitude"];
+
+      if (!(await checkIfUserIsLineOwner(userId, lineId))) {
+        throw new UnauthorizedError("Line does not belong to this user");
+      }
+
+      const memory = await memoryService.createMemory(
+        lineId,
+        title,
+        description,
+        latitude,
+        longitude
+      );
+
+      res.status(200).json({
+        memory,
+      });
     } catch (err) {
       next(err);
     }
   }
 );
 
-// TODO: Verify if correct user.
-router.get("/:memoryId", passport.authenticate(['jwt'], { session: false }), async (req, res, next) => {
-  const memoryId = req.params.memoryId;
-  try {
-    const memories = await memoryService.getMemoryByMemoryId(memoryId);
-    res.status(200).json({
-      memories
-    })
-  } catch (err) {
-    next(err);
-  }
-});
+router.get(
+  "/:memoryId",
+  passport.authenticate(["jwt"], { session: false }),
+  async (req, res, next) => {
+    try {
+      const memoryId = req.params.memoryId;
+      const userId = req.user.userId;
 
-router.delete("/:memoryId", passport.authenticate(['jwt'], { session: false }), async (req, res, next) => {
-  const memoryId = req.params.memoryId;
-  try {
-    const deletedMemory = await memoryService.deleteOneById(memoryId);
-    res.status(200).json({
-      memory: deletedMemory
-    });
-  } catch (err) {
-    next(err);
+      if (!(await checkIfUserIsMemoryOwner(userId, memoryId))) {
+        throw new UnauthorizedError("Memory does not belong to this user");
+      }
+
+      const memories = await memoryService.getMemoryByMemoryId(memoryId);
+      res.status(200).json({
+        memories,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
+
+router.delete(
+  "/:memoryId",
+  passport.authenticate(["jwt"], { session: false }),
+  async (req, res, next) => {
+    try {
+      const memoryId = req.params.memoryId;
+      const userId = req.user.userId;
+
+      if (!(await checkIfUserIsMemoryOwner(userId, memoryId))) {
+        throw new UnauthorizedError("Memory does not belong to this user");
+      }
+
+      const deletedMemory = await memoryService.deleteOneById(memoryId);
+      res.status(200).json({
+        memory: deletedMemory,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.patch(
+  "/:memoryId",
+  passport.authenticate(["jwt"], { session: false }),
+  [
+    oneOf(
+      [
+        check("title", "Title of memory cannot be blank").notEmpty(),
+        check("description", "Description cannot be blank").notEmpty(),
+        check("line", "Line cannot be blank").notEmpty(),
+        check("latitude", "Latitude cannot be blank").notEmpty(),
+        check("longitude", "Longitude cannot be blank").notEmpty(),
+      ],
+      "At least one field must be present"
+    ),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new BadRequestError(
+          errors
+            .array()
+            .map((err) => err.msg)
+            .join(", ")
+        );
+      }
+
+      const userId = req.user.userId;
+      const memoryId = req.params.memoryId;
+      const title = req.body["title"];
+      const lineId = req.body["line"];
+      const description = req.body["description"];
+      const creationDate = req.body["creationDate"];
+      const latitude = req.body["latitude"];
+      const longitude = req.body["longitude"];
+
+      if (!(await checkIfUserIsMemoryOwner(userId, memoryId))) {
+        throw new UnauthorizedError("Memory does not belong to this user");
+      }
+
+      const memory = await memoryService.updateMemory(
+        memoryId,
+        lineId,
+        title,
+        description,
+        creationDate,
+        latitude,
+        longitude
+      );
+
+      res.status(200).json({
+        memory,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+async function checkIfUserIsLineOwner(userId, lineId) {
+  const line = await lineService.getLineByLineId(lineId);
+  return line["userId"] === userId;
+}
+
+async function checkIfUserIsMemoryOwner(userId, memoryId) {
+  const memory = await memoryService.getMemoryByMemoryId(memoryId);
+  const userLines = await lineService.getAllLinesByUserId(userId);
+  memoryLineId = memory["lineId"];
+
+  for (var i = 0; i < userLines.length; i += 1) {
+    line = userLines[i];
+    if (memoryLineId === line["lineId"]) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = router;
--- services/MemoryService.js
+++ services/MemoryService.js
@@ -1,19 +1,55 @@
-const pool = require('../db/db');
-const camelizeKeys = require('../db/utils');
-const {
-  NotFoundError
-} = require('../errors/errors');
+const pool = require("../db/db");
+const camelizeKeys = require("../db/utils");
+const { NotFoundError } = require("../errors/errors");
 
 class MemoryService {
-  constructor() { }
+  constructor() {}
 
-  async createMemory(lineId, title, description, creationDate, latitude, longitude) {
+  async createMemory(lineId, title, description, latitude, longitude) {
     try {
       const newMemory = await pool.query(
-        "INSERT INTO memories (line_id, title, description, creation_date, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-        [lineId, title, description, creationDate, latitude, longitude]
+        "INSERT INTO memories (line_id, title, description, creation_date, latitude, longitude) VALUES ($1, $2, $3, NOW(), $4, $5) RETURNING *",
+        [lineId, title, description, latitude, longitude]
       );
       return camelizeKeys(newMemory.rows[0]);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateMemory(
+    memoryId,
+    lineId,
+    title,
+    description,
+    creationDate,
+    latitude,
+    longitude
+  ) {
+    try {
+      const updatedMemory = await pool.query(
+        `
+        UPDATE memories
+          SET line_id = COALESCE($1, line_id),
+            title = COALESCE($2, title),
+            description = COALESCE($3, description),
+            creation_date = COALESCE($4, creation_date),
+            latitude = COALESCE($5, latitude),
+            longitude = COALESCE($6, longitude)
+          WHERE memory_id = $7
+          RETURNING *
+        `,
+        [
+          lineId,
+          title,
+          description,
+          creationDate,
+          latitude,
+          longitude,
+          memoryId,
+        ]
+      );
+      return camelizeKeys(updatedMemory.rows[0]);
     } catch (err) {
       throw err;
     }
@@ -45,11 +81,12 @@ class MemoryService {
 
   async deleteMemoryById(memoryId) {
     try {
-      const deletedMemory = await pool.query("DELETE FROM memories WHERE memory_id = $1 RETURNING *", [
-        memoryId
-      ]);
+      const deletedMemory = await pool.query(
+        "DELETE FROM memories WHERE memory_id = $1 RETURNING *",
+        [memoryId]
+      );
       if (!deletedMemory.rows[0]) {
-        throw NotFoundError('Memory does not exist, cannot delete');
+        throw NotFoundError("Memory does not exist, cannot delete");
       }
       return camelizeKeys(deletedMemory.rows[0]);
     } catch (err) {
@@ -58,4 +95,4 @@ class MemoryService {
   }
 }
 
-module.exports = MemoryService
+module.exports = MemoryService;
